fc9506d653b4eb0cbf22c9eb10b66e8bd6db0626
Felipe Augusto <felipe.dossantos@fluig.com>
2018-12-02 17:04:07 -0200

    changes in general

--- topology.cc
+++ topology.cc
@@ -14,6 +14,14 @@ int main(int argc, char *argv[])
   CommandLine cmd;
   cmd.Parse (argc, argv);
 
+  double deviceDataRate = 0.5;
+  double sharedLinkDataRate = deviceDataRate/2;
+  double dedicatedLinkDataRate = deviceDataRate;
+
+  std::string stringDeviceDataRate = std::to_string(deviceDataRate) + "Mbps";
+  std::string stringSharedLinkDataRate = std::to_string(sharedLinkDataRate) + "Mbps";
+  std::string stringDedicatedLinkDataRate = std::to_string(dedicatedLinkDataRate) + "Mbps";
+
   /* Configuration. */
 
   /* Build nodes. */
@@ -31,21 +39,35 @@ int main(int argc, char *argv[])
   term_4.Create (1);
 
   /* Build link. */
+  //link between sender 0 and the first router
   CsmaHelper csma_hub_0;
-  csma_hub_0.SetChannelAttribute ("DataRate", DataRateValue (100000000));
+  csma_hub_0.SetChannelAttribute ("DataRate", StringValue(stringDedicatedLinkDataRate));
   csma_hub_0.SetChannelAttribute ("Delay", StringValue ("5ms"));
+  //sender 0
+  //term_1.SetDeviceAttribute ("DataRate", StringValue(stringDeviceDataRate));
+
+  //link between receiver 1 and the first router
   CsmaHelper csma_hub_1;
-  csma_hub_1.SetChannelAttribute ("DataRate", DataRateValue (100000000));
+  csma_hub_1.SetChannelAttribute ("DataRate", StringValue(stringDedicatedLinkDataRate));
   csma_hub_1.SetChannelAttribute ("Delay", StringValue ("5ms"));
+  
+  //link between sender 2 and the first router
   CsmaHelper csma_hub_2;
-  csma_hub_2.SetChannelAttribute ("DataRate", DataRateValue (100000000));
+  csma_hub_2.SetChannelAttribute ("DataRate", StringValue(stringDedicatedLinkDataRate));
   csma_hub_2.SetChannelAttribute ("Delay", StringValue ("5ms"));
+  
+  //link between routers
   CsmaHelper csma_hub_3;
-  csma_hub_3.SetChannelAttribute ("DataRate", DataRateValue (100000000));
+  csma_hub_3.SetChannelAttribute ("DataRate", StringValue(stringSharedLinkDataRate));
   csma_hub_3.SetChannelAttribute ("Delay", StringValue ("5ms"));
+  
+  //link between sender 4 and the first router
   CsmaHelper csma_hub_4;
-  csma_hub_4.SetChannelAttribute ("DataRate", DataRateValue (100000000));
+  csma_hub_4.SetChannelAttribute ("DataRate", StringValue(stringDedicatedLinkDataRate));
   csma_hub_4.SetChannelAttribute ("Delay", StringValue ("5ms"));
+
+  //sender 4
+  //term_4.SetDeviceAttribute ("DataRate", StringValue(stringDeviceDataRate));
 
   /* Build link net device container. */
   NodeContainer all_hub_0;
@@ -99,25 +121,57 @@ int main(int argc, char *argv[])
   Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
 
   /* Generate Application. */
-  uint16_t port_tcp_0 = 1;
-  Address sinkLocalAddress_tcp_0 (InetSocketAddress (Ipv4Address::GetAny (), port_tcp_0));
-  PacketSinkHelper sinkHelper_tcp_0 ("ns3::TcpSocketFactory", sinkLocalAddress_tcp_0);
-  ApplicationContainer sinkApp_tcp_0 = sinkHelper_tcp_0.Install (term_3);
-  sinkApp_tcp_0.Start (Seconds (0.0));
-  sinkApp_tcp_0.Stop (Seconds (1.0));
-  OnOffHelper clientHelper_tcp_0 ("ns3::TcpSocketFactory", Address ());
-  clientHelper_tcp_0.SetAttribute ("OnTime", StringValue ("ns3::ConstantRandomVariable[Constant=1]"));
-  clientHelper_tcp_0.SetAttribute ("OffTime", StringValue ("ns3::ConstantRandomVariable[Constant=0]"));
-  ApplicationContainer clientApps_tcp_0;
-  AddressValue remoteAddress_tcp_0 (InetSocketAddress (iface_ndc_hub_2.GetAddress (1), port_tcp_0));
-  clientHelper_tcp_0.SetAttribute ("Remote", remoteAddress_tcp_0);
-  clientApps_tcp_0.Add (clientHelper_tcp_0.Install (term_4));
-  clientApps_tcp_0.Start (Seconds (0.0));
-  clientApps_tcp_0.Stop (Seconds (1.0));
-  uint16_t port_tcp_1 = 3;
+  uint16_t port_tcp_3 = 3;
+  Address sinkLocalAddress_tcp_3 (InetSocketAddress (Ipv4Address::GetAny (), port_tcp_3));
+  PacketSinkHelper sinkHelper_tcp_3 ("ns3::TcpSocketFactory", sinkLocalAddress_tcp_3);
+  ApplicationContainer sinkApp_tcp_3 = sinkHelper_tcp_3.Install (term_3);
+  sinkApp_tcp_3.Start (Seconds (0.0));
+  sinkApp_tcp_3.Stop (Seconds (1.0));
+  OnOffHelper clientHelper_tcp_3 ("ns3::TcpSocketFactory", Address ());
+  clientHelper_tcp_3.SetAttribute ("OnTime", StringValue ("ns3::ConstantRandomVariable[Constant=1]"));
+  clientHelper_tcp_3.SetAttribute ("OffTime", StringValue ("ns3::ConstantRandomVariable[Constant=0]"));
+  ApplicationContainer clientApps_tcp_3;
+  AddressValue remoteAddress_tcp_3 (InetSocketAddress (iface_ndc_hub_2.GetAddress (1), port_tcp_3));
+  clientHelper_tcp_3.SetAttribute ("Remote", remoteAddress_tcp_3);
+  clientApps_tcp_3.Add (clientHelper_tcp_3.Install (term_4));
+  clientApps_tcp_3.Start (Seconds (0.0));
+  clientApps_tcp_3.Stop (Seconds (1.0));
+  uint16_t port_tcp_4 = 4;
+  Address sinkLocalAddress_tcp_4 (InetSocketAddress (Ipv4Address::GetAny (), port_tcp_4));
+  PacketSinkHelper sinkHelper_tcp_4 ("ns3::TcpSocketFactory", sinkLocalAddress_tcp_4);
+  ApplicationContainer sinkApp_tcp_4 = sinkHelper_tcp_4.Install (term_3);
+  sinkApp_tcp_4.Start (Seconds (1.0));
+  sinkApp_tcp_4.Stop (Seconds (2.0));
+  OnOffHelper clientHelper_tcp_4 ("ns3::TcpSocketFactory", Address ());
+  clientHelper_tcp_4.SetAttribute ("OnTime", StringValue ("ns3::ConstantRandomVariable[Constant=1]"));
+  clientHelper_tcp_4.SetAttribute ("OffTime", StringValue ("ns3::ConstantRandomVariable[Constant=0]"));
+  ApplicationContainer clientApps_tcp_4;
+  AddressValue remoteAddress_tcp_4 (InetSocketAddress (iface_ndc_hub_2.GetAddress (1), port_tcp_4));
+  clientHelper_tcp_4.SetAttribute ("Remote", remoteAddress_tcp_4);
+  clientApps_tcp_4.Add (clientHelper_tcp_4.Install (term_4));
+  clientApps_tcp_4.Start (Seconds (1.0));
+  clientApps_tcp_4.Stop (Seconds (2.0));
+
+  /* Generate Application. */
+  uint16_t port_tcp_2 = 2;
+  Address sinkLocalAddress_tcp_2 (InetSocketAddress (Ipv4Address::GetAny (), port_tcp_2));
+  PacketSinkHelper sinkHelper_tcp_2 ("ns3::TcpSocketFactory", sinkLocalAddress_tcp_2);
+  ApplicationContainer sinkApp_tcp_2 = sinkHelper_tcp_2.Install (term_2);
+  sinkApp_tcp_2.Start (Seconds (0.0));
+  sinkApp_tcp_2.Stop (Seconds (1.0));
+  OnOffHelper clientHelper_tcp_2 ("ns3::TcpSocketFactory", Address ());
+  clientHelper_tcp_2.SetAttribute ("OnTime", StringValue ("ns3::ConstantRandomVariable[Constant=1]"));
+  clientHelper_tcp_2.SetAttribute ("OffTime", StringValue ("ns3::ConstantRandomVariable[Constant=0]"));
+  ApplicationContainer clientApps_tcp_2;
+  AddressValue remoteAddress_tcp_2 (InetSocketAddress (iface_ndc_hub_2.GetAddress (1), port_tcp_2));
+  clientHelper_tcp_2.SetAttribute ("Remote", remoteAddress_tcp_2);
+  clientApps_tcp_2.Add (clientHelper_tcp_2.Install (term_1));
+  clientApps_tcp_2.Start (Seconds (0.0));
+  clientApps_tcp_2.Stop (Seconds (1.0));
+  uint16_t port_tcp_1 = 1;
   Address sinkLocalAddress_tcp_1 (InetSocketAddress (Ipv4Address::GetAny (), port_tcp_1));
   PacketSinkHelper sinkHelper_tcp_1 ("ns3::TcpSocketFactory", sinkLocalAddress_tcp_1);
-  ApplicationContainer sinkApp_tcp_1 = sinkHelper_tcp_1.Install (term_3);
+  ApplicationContainer sinkApp_tcp_1 = sinkHelper_tcp_1.Install (term_2);
   sinkApp_tcp_1.Start (Seconds (1.0));
   sinkApp_tcp_1.Stop (Seconds (2.0));
   OnOffHelper clientHelper_tcp_1 ("ns3::TcpSocketFactory", Address ());
@@ -126,7 +180,7 @@ int main(int argc, char *argv[])
   ApplicationContainer clientApps_tcp_1;
   AddressValue remoteAddress_tcp_1 (InetSocketAddress (iface_ndc_hub_2.GetAddress (1), port_tcp_1));
   clientHelper_tcp_1.SetAttribute ("Remote", remoteAddress_tcp_1);
-  clientApps_tcp_1.Add (clientHelper_tcp_1.Install (term_4));
+  clientApps_tcp_1.Add (clientHelper_tcp_1.Install (term_1));
   clientApps_tcp_1.Start (Seconds (1.0));
   clientApps_tcp_1.Stop (Seconds (2.0));
 
@@ -135,6 +189,7 @@ int main(int argc, char *argv[])
   /* Stop the simulation after x seconds. */
   uint32_t stopTime = 3;
   Simulator::Stop (Seconds (stopTime));
+  
   /* Start and clean simulation. */
   AnimationInterface anim("anim1.xml");
   anim.SetConstantPosition(term_1.Get(0),1.0,2.0);
